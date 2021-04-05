926c337c6d30ee4b89466f7ebaaabf11608ecd48
Robert Rousset <whyisrob@hotmail.com>
2021-04-05 23:53:09 +0800

    updated version, creating elements with Javascript but local storage and color changes not working yet

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,76 @@
-var currentDay = document.getElementById('currentDay')
-var hour = document.querySelector('.hour')
-var container = document.getElementsByClassName('container')
-var content = document.getElementById('description')
-var saveBTN = document.querySelector('.saveBtn')
 var currentTime = moment().hour();
-var row = document.querySelector('.row')
+
+$(document).ready(function(){
+  //ADDING CURRENT DAY AT THE TOP OF THE PAGE
+  var currentDay = $('#currentDay')
+  currentDay.text(moment().format('MMMM DD'))
+  
+  //GETTING THE CONTAINER//
+  var container = $('.container')
+
+  //THIS IS TO GET THE STRUCTURE OF A ROW FOR THE AMOUNT OF HOURS//
+  for (let hours = 8; hours <= 17; hours++) {
+  var row = $('<div>')
+  row.addClass('row')
+  container.append(row)
+  
+  //THIS MAKES THE TEXT IN THE HOUR SECTIONS REPRESENT THE RIGHT TIME//
+  var hour = $('<div>')
+  hour.addClass('hour col col-lg-2')
+  hour.text(hours + ":00AM")
+  if(hours === 12){
+    hour.text(hours + ":00PM")
+  }
+  if (hours > 12){
+    hour.text(hours -12 + ":00PM")
+  }
+  row.append(hour)
+
+  //THIS IS THE TEXT AREA//
+  var userInput = $('<textarea>')
+  userInput.addClass('col').attr('id', 'description')
+  row.append(userInput)
+
+  //FOR THE CHANGING COLOURS//
+  var content = $('textarea')
+    if(hours  < currentTime){
+      $(content).addClass('past')
+      $(content).removeClass('future')
+      $(content).removeClass('present')
+    }
+    if(hours === currentTime){
+      $(content).removeClass('past')
+      $(content).addClass('future')
+      $(content).removeClass('present')
+    }
+    if(hours > currentTime){
+      $(content).removeClass('past')
+      $(content).removeClass('future')
+      $(content).addClass('present')
+    }
+
+  //SAVE BUTTON//
+  var button = $('<button>')
+  button.addClass('saveBtn col col-lg-2')
+  row.append(button)
+
+  //CURRENT TIME//
 
 
-currentDay.textContent = moment().format('MMMM DD')
+  //MAKING THE SAVED CONTENT APPEAR AFTER REFRESH//
+  
 
-saveBTN.addEventListener("click", function(){
-  var userText = content.value;
-  localStorage.setItem("dailyPlan", userText)
+  //MAKING THE BUTTON SAVE THE USERS CONTENT TO LOCAL STORAGE//
+  button.on("click", function(){
+    var userText = $('#description').val()
+    localStorage.setItem(hours, userText)
+    console.log(userText)
+    userInput.hours = localStorage.getItem(hours, userText)
+  })
+  }
 })
 
-var dailyPlan = localStorage.getItem("dailyPlan")
-content.textContent = dailyPlan;
 
-function updateContentBoxColour() {
-  var timeInHour = hour.textContent
 
-  if(timeInHour < currentTime){
-    $(content).addClass('past')
-    $(content).removeClass('future')
-    $(content).removeClass('present')
-  }
-  if(timeInHour == currentTime){
-    $(content).removeClass('past')
-    $(content).addClass('future')
-    $(content).removeClass('present')
-  }
-  if(timeInHour > currentTime){
-    $(content).removeClass('past')
-    $(content).removeClass('future')
-    $(content).addClass('present')
-  }
-}
 
-updateContentBoxColour();
 
